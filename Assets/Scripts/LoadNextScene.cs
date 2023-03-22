251821bd648ada2fd55cce887826109e6239c266
Shiva <patel_shiva23@hotmail.com>
2023-03-22 10:07:08 -0400

    LoadNextScene(Level)
    - LoadNextScene object with script, on triggerEnter will load next scene

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+    [SerializeField] string _sceneName;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        var player = collision.GetComponent<Unit>();
+        if (player == null)
+            return;
+
+        //Add Coroutine to animate Flag before loading next scene
+        StartCoroutine(LoadAfterDelay());
+    }
+
+    IEnumerator LoadAfterDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        // load new level
+        SceneManager.LoadScene(_sceneName);
+
+    }
+}
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Testing : MonoBehaviour
 {
@@ -19,4 +21,6 @@ public class Testing : MonoBehaviour
             
         }
     }
+
+  
 }
